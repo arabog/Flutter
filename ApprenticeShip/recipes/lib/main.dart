defc6f268d183da0b7259e2529cee092b0a8de7e
arabambi <arababs2015@gmail.com>
2022-01-06 18:45:22 +0100

    chapter 2 completed

--- ApprenticeShip/recipes/lib/main.dart
+++ ApprenticeShip/recipes/lib/main.dart
@@ -1,106 +1,115 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
-
+import "recipe_detail.dart";
 
 void main() {
-	runApp(const RecipeApp());
+  runApp(const RecipeApp());
 }
-
 
 class RecipeApp extends StatelessWidget {
-	const RecipeApp({Key? key}) : super(key: key);
+  const RecipeApp({Key? key}) : super(key: key);
 
-	// This widget is the root of your application.
-	@override
-	Widget build(BuildContext context) {
-		final ThemeData theme = ThemeData();
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
 
-		return MaterialApp(
-		title: 'Recipe Calculator',
-		theme: theme.copyWith(
-
-			colorScheme: theme.colorScheme.copyWith(
-				primary: Colors.grey,
-				secondary: Colors.black, // Your accent color
-			),
-		),
-		home: const MyHomePage(title: 'Recipe Calculator'),
-		);
-	}
+    return MaterialApp(
+      title: 'Recipe Calculator',
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.grey,
+          secondary: Colors.black, // Your accent color
+        ),
+      ),
+      home: const MyHomePage(title: 'Recipe Calculator'),
+    );
+  }
 }
-
 
 class MyHomePage extends StatefulWidget {
-	const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
+  final String title;
 
-	final String title;
-
-	@override
-	State<MyHomePage> createState() => _MyHomePageState();
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
-
 
 class _MyHomePageState extends State<MyHomePage> {
-	@override
-	Widget build(BuildContext context) {
-		// 1
-		return Scaffold(
-			// 2
-			appBar: AppBar(
-				title: Text(widget.title),
-			),
-			// 3
-			body: SafeArea(
-				// 4
-				child: ListView.builder(
-					// 5
-					itemCount: Recipe.samples.length,
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return Scaffold(
+      // 2
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // 3
+      body: SafeArea(
+        // 4
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
 
-					// 6
-					itemBuilder: (BuildContext context, int index) {
-					// 7
-					// TODO: Add GestureDetector
-						return buildRecipeCard(Recipe.samples[index]);
-					},
-				),
-			),
-		);
-	}
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            // TODO: Add GestureDetector
+            // 7
+            return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      // 10
+                      // TODO: Replace return with return RecipeDetail()
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              // 11
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
-
 
 Widget buildRecipeCard(Recipe recipe) {
-	return Card(
-		// 1
-		elevation: 2.0,
-		// 2
-		shape: RoundedRectangleBorder(
-			borderRadius: BorderRadius.circular(10.0)
-		),
-			// 3
-		child: Padding(
-			padding: const EdgeInsets.all(16.0),
-			// 4
-			child: Column(
-				children: <Widget>[
-					Image(image: AssetImage(recipe.imageUrl)),
-					// 5
-					const SizedBox(
-						height: 14.0,
-					),
-					// 6
-					Text(
-						recipe.label,
-						style: const TextStyle(
-							fontSize: 20.0,
-							fontWeight: FontWeight.w700,
-							fontFamily: 'Palatino',
-						),
-					)
-				],
-			),
-		),
-	);
+  return Card(
+    // 1
+    elevation: 2.0,
+    // 2
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+    // 3
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      // 4
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          // 5
+          const SizedBox(
+            height: 14.0,
+          ),
+          // 6
+          Text(
+            recipe.label,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Palatino',
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
-
