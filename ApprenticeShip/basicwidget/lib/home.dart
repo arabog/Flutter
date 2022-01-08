6f86ebf6fa8fed5f0cd46c59b7a47bb4d5d0c569
arabambi <arababs2015@gmail.com>
2022-01-08 11:31:58 +0100

    card3 completed

--- ApprenticeShip/basicwidget/lib/home.dart
+++ ApprenticeShip/basicwidget/lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'card1.dart';
 import 'card2.dart';
+import 'card3.dart';
 
 
 // 1
@@ -25,8 +26,9 @@ class _HomeState extends State<Home> {
      const Card2(),
     // Container(color: Colors.green),
 
-    // todo: replace with card1
-    Container(color: Colors.blue),
+    // todo: replace with card3
+    const Card3()
+    // Container(color: Colors.blue),
   ];
 
   // 9
