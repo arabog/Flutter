9f78bdb579363d4d3329463f775bd253c7d5f131
arabambi <arababs2015@gmail.com>
2022-01-23 07:28:37 +0100

    app_link.dart added

---
+++ ApprenticeShip/deep_links_and_web_urls/lib/navigation/app_link.dart
@@ -0,0 +1,57 @@
+class AppLink {
+  // 1
+  static const String kHomePath = '/home';
+  static const String kOnboardingPath = '/onboarding';
+  static const String kLoginPath = '/login';
+  static const String kProfilePath = '/profile';
+  static const String kItemPath = '/item';
+
+  // 2
+  static const String kTabParam = 'tab';
+  static const String kIdParam = 'id';
+
+// Converting the app state to a URL string:
+  // 3
+  String? location;
+  // 4
+  int? currentTab;
+  // 5
+  String? itemId;
+  // 6
+  AppLink({
+    this.location,
+    this.currentTab,
+    this.itemId,
+  });
+
+  // Converting a URL string to an AppLink:
+  // TODO: Add fromLocation
+  static AppLink fromLocation(String? location) {
+    // 1
+    location = Uri.decodeFull(location ?? '');
+
+    // 2
+    final uri = Uri.parse(location);
+    final params = uri.queryParameters;
+
+    // 3
+    final currentTab = int.tryParse(params[AppLink.kTabParam] ?? '');
+
+    // 4
+    final itemId = params[AppLink.kIdParam];
+
+    // 5
+    final link = AppLink(
+      location: uri.path,
+      currentTab: currentTab,
+      itemId: itemId,
+    );
+
+    // 6
+    return link;
+
+    
+  }
+
+// TODO: Add toLocation
+}
