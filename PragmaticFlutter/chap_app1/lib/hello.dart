3ad2bc0ac8ce1ee0b00b073f0343a8a86b089113
arabambi <arababs2015@gmail.com>
2022-02-21 10:14:15 +0100

    updated

--- PragmaticFlutter/chap_app1/lib/hello.dart
+++ PragmaticFlutter/chap_app1/lib/hello.dart
@@ -47,8 +47,6 @@ a value to a variable similar to the `var` keyword. The difference between
 the two is that for the `dynamic` keyword, the same variable can be 
 reassigned to a different data type
 
-*/ 
-
 void main() {
 	var data = 1;
 
@@ -58,7 +56,6 @@ void main() {
 	dynamic dynamicData = 1;
 	print(dynamicData.runtimeType);		// int
 
-
 	// re-assigning data to String data type
 	dynamicData = "I'm a string now";
 	print(dynamicData.runtimeType);	// Srting
@@ -66,4 +63,70 @@ void main() {
 }
 
 
+COLLECTIONS:
 
+LIST:
+Dart’s List (List<E> class) is a collection that holds indexable objects. 
+An empty list can is declared using two square brackets ‘[]’. List items 
+can be put inside these brackets separated by commas.
+
+void main() {
+	List emptyList = [];
+
+	// checking if list is empty
+	var result = emptyList.isEmpty;
+
+	print(result);	// true
+}
+
+SPREAD OPERATOR
+Dart 2.3 introduced the spread operator and null aware spread
+operator to combine multiple lists into one.
+Combining multiple lists can be done using spread (...) and 
+null aware spread (?...) operators.
+
+
+List emptyList = [];
+
+List theList = ['Dart', 'Kotlin'];
+
+// null list
+List? theNullList;
+
+/*
+	Spread operator flatten d list for it to be mergeable to anoda list
+	Null spread operator helps avoid runtime exceptions - iterator
+	called on null
+*/ 
+
+List anodaList = ['Java', ...theList, ...?theNullList];
+
+void main() {
+	dynamic result;
+
+	result = emptyList.isEmpty;
+	print(result);
+	
+	print(anodaList);	// [Java, Dart, Kotlin]
+
+}
+
+TRANSFORM LIST ITEMS
+The `map` on the `List` can be called to transform the list items. Let’s 
+append the word ‘Language’ to objects of the `theList` list.
+
+List theList = ['Dart', 'Kotlin'];
+
+void main() {
+	dynamic result;
+
+	result = theList.map((e) => "$e Language").toList();
+
+	print(result);	// [Dart Language, Kotlin Language]
+}
+
+FILTERING
+The `where` method on `List` can help filter items meeting specific criteria.
+
+
+*/ 
