69dd31d83d1c3ec928fec89fbaa32d3cf3c2eb0b
arabambi <arababs2015@gmail.com>
2022-03-08 21:27:51 +0100

    Prag chap 13 7th

---
+++ PragmaticFlutter/chaps_app/lib/data_modelling13/booktile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BookTile extends StatelessWidget { 
+	final book;
+	const BookTile({Key key, this.book}) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) {
+		//Card widget
+		//Card's child is Padding
+		//Padding's child is Row
+		//Row's children are Flexible and Image
+		//Flexible's child is Column
+		//Column's children are two Text widgets
+		
+		return Card(
+			child: Padding(
+				child: Row(
+					children: [
+						Flexible(
+							child: Column(
+								children: <Widget>[
+									Text(),
+									
+									Text(),
+								],
+							),
+						),
+
+						Image.network()
+					],
+				),
+			),
+		);
+	}
+}
