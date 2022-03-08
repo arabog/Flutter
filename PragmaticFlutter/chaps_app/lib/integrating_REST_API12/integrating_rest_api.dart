a9e0716ba7d00e46cbbbd9f891ab1cd237781827
arabambi <arababs2015@gmail.com>
2022-03-08 09:30:50 +0100

    chap 12 update 4

--- PragmaticFlutter/chaps_app/lib/integrating_REST_API12/integrating_rest_api.dart
+++ PragmaticFlutter/chaps_app/lib/integrating_REST_API12/integrating_rest_api.dart
@@ -202,43 +202,92 @@ class _BookListingState extends State<BookListing> {
 }
 
 
-DiSPlaying Data in aPP
-Now that we’ve remote data fetched and updated in the `booksResponse` variable, we are
-ready to display it in our interface. Since this data is a large blob of text, we need a widget
-that can adapt to scrolling if needed. The `SingleChildScrollView` widget can help with this
-requirement. It can accommodate a large blob of text in a scrolling manner. As soon as we
-start the app, it displays a message: ‘No Response from API’ onscreen since no data is
-available yet. The remote data is being fetched using the `await makeHttpCall()`
-method. The variable `booksResponse` is updated in the `setState()` method. The `setState`
-method triggers rebuilding the interface with newly fetched data, and a large text blob is
-displayed on the screen.
-import 'package:flutter/material.dart'; import 'package:http/http.dart' as http; import
-'../config.dart'; //Making HTTP request Future<String> makeHttpCall() async { final apiKey =
-"$YOUR_API_KEY"; final apiEndpoint =
-"https://www.googleapis.com/books/v1/
-volumes?key=$apiKey&q=python"; final http.Response response =
-await
-http.get(apiEndpoint, headers: {'Accept': 'application/json'}); //This will print `flutter: Instance
-of 'Response'` on console. print(response); return response.body; }
-class BooksApp extends StatelessWidget { @override Widget build(BuildContext context) {
-return MaterialApp(
-debugShowCheckedModeBanner: false,
-home: BookListing(), );
-} } class BookListing extends StatefulWidget { @override _BookListingState createState()
-=> _BookListingState(); } class _BookListingState extends State<BookListing> { String
-booksResponse; //method to fetch books asynchronously fetchBooks() async { //making
-REST API call var response = await makeHttpCall(); //Updating booksResponse to
-fetched remote data setState(() {
-booksResponse = response; }); } @override void
-initState() { super.initState(); fetchBooks(); } @override Widget build(BuildContext
-context) { //fetching books listing //fetchBooks(); return Scaffold(
-body:
-SingleChildScrollView(
-child: booksResponse != null
-? Text("Google Books API
-response\n $booksResponse")
-: Text("No Response from API"),
-), ); } }
-In the Modeling, you will learn about parsing JSON data and create object models to better
-manage code.
+Displaying Data in App
+Now that we’ve remote data fetched and updated in the `booksResponse` 
+variable, we are ready to display it in our interface. Since this data is a 
+large blob of text, we need a widget that can adapt to scrolling if needed. 
+
+The `SingleChildScrollView` widget can help with this requirement. It can 
+accommodate a large blob of text in a scrolling manner. As soon as we start 
+the app, it displays a message: ‘No Response from API’ onscreen since no 
+data is available yet. The remote data is being fetched using the 
+`await makeHttpCall()` method. The variable `booksResponse` is updated 
+in the `setState()` method. The `setState` method triggers rebuilding the 
+interface with newly fetched data, and a large text blob is displayed on 
+the screen.
+
+import 'package:flutter/material.dart'; 
+import 'package:http/http.dart' as http; 
+import '../config.dart'; 
+
+//Making HTTP request 
+Future<String> makeHttpCall() async { 
+	final apiKey = "$YOUR_API_KEY"; 
+	final apiEndpoint = "https://www.googleapis.com/books/v1/volumes?key=$apiKey&q=python"; 
+
+	final http.Response response =
+	await http.get(apiEndpoint, headers: {'Accept': 'application/json'});
+	
+	 //This will print `flutter: Instance of 'Response'` on console. 
+	print(response); 
+	return response.body; 
+}
+
+
+class BooksApp extends StatelessWidget { 
+	@override 
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			debugShowCheckedModeBanner: false,
+
+			home: BookListing(), 
+		);
+	} 
+} 
+
+
+class BookListing extends StatefulWidget { 
+	@override 
+	_BookListingState createState() => _BookListingState(); 
+} 
+
+
+class _BookListingState extends State<BookListing> { 
+	String booksResponse; 
+
+	//method to fetch books asynchronously 
+	fetchBooks() async { 
+		//making REST API call 
+		var response = await makeHttpCall(); 
+
+	//Updating booksResponse to fetched remote data 
+	setState(() {
+		booksResponse = response; }); 
+	} 
+
+	@override 
+	void initState() { 
+		super.initState(); 
+		
+		fetchBooks(); 
+	} 
+
+	@override 
+	Widget build(BuildContext context) { 
+		//fetching books listing 
+		fetchBooks(); 
+		
+		return Scaffold(
+			body: SingleChildScrollView(
+				child: booksResponse != null
+					? Text("Google Books API response\n $booksResponse")
+					: Text("No Response from API"),
+			), 
+		); 
+	} 
+}
+
+In the Modeling, you will learn about parsing JSON data and create object 
+models to better manage code.
 */ 
+
