bf699ce5d634e14d785c934410399248ea461e55
arabambi <arababs2015@gmail.com>
2022-03-10 09:41:23 +0100

    chap 14 init

---
+++ PragmaticFlutter/chaps_app/lib/navigatn_and_routing14/navigatn_and_routing.dart
@@ -0,0 +1,32 @@
+/*
+Navigation and Routing
+In this chapter, we will learn about the three types of navigation 
+and routing to implement navigation from the BookListing homepage 
+to the BookDetailsPage screen. The BookDetailsPage page only 
+displays the book details
+
+SIMPLE BookDetailsPage SCREEN
+In this section, you will learn to build a basic secondary page 
+widget – BookDetailsPage, which is used as a placeholder to 
+understand navigation and routing concepts in Flutter application.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/ 
